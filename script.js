0fe34e3e67b9ac942ca2ea2941b1a1db97177b5f
NatCardonaRi <natalia.cardona9@udea.edu.co>
2022-12-12 09:34:18 -0500

    Sprint 1 finalizado

---
+++ script.js
@@ -0,0 +1,28 @@
+// const labels = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun',]
+// const colors = ['#EA755E', '#EA755E', '#B6DEE4', '#F29887', '#EA755E', '#EA755E', '#EA755E'];
+
+// const graph = document.querySelector("#grafica");
+
+// const data = {
+// 	labels: labels,
+// 	datasets: [{
+// 		data: [4, 6, 10, 5, 4, 8, 4],
+// 		backgroundColor: 'colors'
+// 	}]
+// };
+
+// const config = {
+// 	type: 'bar',
+// 	data: data,
+// 	};
+// new Chart(graph, config);
+
+const chart-wrap {
+    --chart-width:420px;
+    --grid-color:#aaa;
+    --bar-color:#F16335;
+    --bar-thickness:40px;
+    --bar-rounded: 3px;
+    --bar-spacing:10px;
+    width:var(--chart-width);
+}
